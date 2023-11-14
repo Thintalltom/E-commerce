b3406fffa56b3043eaf3cb2a97c460c2f081aa65
tomide <tomideadeyanju@gmail.com>
2023-11-14 01:18:16 +0100

    started a new e-commerce project using an external api

---
+++ E-commerce/src/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-around bg-red-500 p-4'>
+        <div>
+            <p>E-shop</p>
+        </div>
+        <p>Home</p>
+        <p>Product</p>
+        <p>Service</p>
+        <p>Conduct</p>
+
+    </div>
+  )
+}
+
+export default Navbar
